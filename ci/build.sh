623cc94bbe87aa872b9dfad9e95cba1d3b515505
Ben Hale <bhale@pivotal.io>
2016-07-13 15:49:48 -0700

    Build and Upload
    This change updates the build scripts to build and upload to our Artifactory
    instance.  It also updates the docker image so that trusty can do the
    uploading.

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
-pushd jvmkill
-  make
-popd
+export JFROG_CLI_OFFER_CONFIG=false
+
+cd jvmkill
+make
+/usr/local/bin/jfrog rt upload \
+  --url https://repo.spring.io \
+  --user $ARTIFACTORY_USERNAME \
+  --password $ARTIFACTORY_PASSWORD \
+  libjvmkill.so \
+  $ARTIFACTORY_REPOSITORY/org/cloudfoundry/jvmkill/$VERSION/libjvmkill-$(echo $VERSION | sed "s|SNAPSHOT|$(date '+%Y%m%d.%H%M%S')|")-$PLATFORM.so
