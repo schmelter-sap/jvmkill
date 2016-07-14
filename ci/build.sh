b100659245ed55c4f23a42ee02e927c250630fbf
Ben Hale <bhale@pivotal.io>
2016-07-14 10:47:15 -0700

    Create Release Script
    This change adds a release creation script to the CI area.

--- ci/build.sh
+++ ci/build.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-export JFROG_CLI_OFFER_CONFIG=false
-
 cd jvmkill
 make
-/usr/local/bin/jfrog rt upload \
+JFROG_CLI_OFFER_CONFIG=false /usr/local/bin/jfrog rt upload \
   --url https://repo.spring.io \
   --user $ARTIFACTORY_USERNAME \
   --password $ARTIFACTORY_PASSWORD \
