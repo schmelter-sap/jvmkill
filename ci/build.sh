f7ef93630a007f8f5a42d84f9c6b8c73b4e83fa1
Ben Hale <bhale@pivotal.io>
2017-01-25 15:14:48 -0800

    Normalize Maven Artifact names

--- ci/build.sh
+++ ci/build.sh
@@ -9,4 +9,4 @@ JFROG_CLI_OFFER_CONFIG=false /usr/local/bin/jfrog rt upload \
   --user $ARTIFACTORY_USERNAME \
   --password $ARTIFACTORY_PASSWORD \
   libjvmkill.so \
-  $ARTIFACTORY_REPOSITORY/org/cloudfoundry/jvmkill/$VERSION/libjvmkill-$(echo $VERSION | sed "s|SNAPSHOT|$(date '+%Y%m%d.%H%M%S')|")-$PLATFORM.so
+  $ARTIFACTORY_REPOSITORY/org/cloudfoundry/jvmkill/$VERSION/jvmkill-$(echo $VERSION | sed "s|SNAPSHOT|$(date '+%Y%m%d.%H%M%S')|")-$PLATFORM.so
