ce655581071512b5aeaa98f57f5a212da4b8643b
Glyn Normington <gnormington@pivotal.io>
2017-05-08 15:58:07 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,34 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-include!(concat!(env!("OUT_DIR"), "/jvmti_bindings.rs"));
+use std::ptr;
+
+extern crate libc;
+
+#[allow(dead_code)]
+mod jvmti {
+    include!(concat!(env!("OUT_DIR"), "/jvmti_bindings.rs"));
+}
+
+#[no_mangle]
+#[allow(non_snake_case, unused_variables)]
+pub extern fn Agent_OnLoad(vm: *mut jvmti::JavaVM, options: *mut ::std::os::raw::c_char,
+                           reserved: *mut ::std::os::raw::c_void) -> jvmti::jint {
+    unsafe {
+        //        pub GetEnv: ::std::option::Option<unsafe extern "C" fn(vm: *mut JavaVM,
+        //                                                               penv:
+        //                                                               *mut *mut ::std::os::raw::c_void,
+        //                                                               version: jint)
+        let mut penv: *mut ::std::os::raw::c_void = ptr::null_mut();
+        (**vm).GetEnv.unwrap()(vm, &mut penv, jvmti::JVMTI_VERSION as i32);
+    }
+
+    0
+}
+
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
