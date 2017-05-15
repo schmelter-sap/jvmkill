43de55cbb8aa1c1def8f9c4f18c5e332fa44c40c
Glyn Normington <gnormington@pivotal.io>
2017-05-15 13:01:14 +0100

    start to structure the code

---
+++ src/env/mod.rs
@@ -0,0 +1,32 @@
+use ::std::io::Write;
+use ::std::ptr;
+use ::jvmti::jvmtiEnv;
+
+pub trait JVMTI {}
+
+pub struct JVMTIEnv {
+    jvmti: *mut jvmtiEnv
+}
+
+macro_rules! errln (
+    ($($arg:tt)*) => { {
+        writeln!(&mut ::std::io::stderr(), $($arg)*).unwrap();
+    } }
+);
+
+impl JVMTIEnv {
+    pub fn new(vm: *mut ::jvmti::JavaVM) -> Result<JVMTIEnv, ::jvmti::jint> {
+        let mut penv: *mut ::std::os::raw::c_void = ptr::null_mut();
+        let rc;
+        unsafe {
+            rc = (**vm).GetEnv.unwrap()(vm, &mut penv, ::jvmti::JVMTI_VERSION as i32);
+        }
+        if rc as u32 != ::jvmti::JNI_OK {
+            errln!("ERROR: GetEnv failed: {}", rc);
+            return Err(::jvmti::JNI_ERR);
+        }
+        Ok(JVMTIEnv { jvmti: penv as *mut jvmtiEnv })
+    }
+}
+
+impl JVMTI for JVMTIEnv {}
--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,19 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use std::ptr;
+mod env;
+mod jvmti;
 
 extern crate libc;
 
-#[allow(dead_code)]
-mod jvmti {
-    include!(concat!(env!("OUT_DIR"), "/jvmti_bindings.rs"));
-}
-
 #[no_mangle]
-#[allow(non_snake_case, unused_variables)]
+#[allow(unused_variables)]
 pub extern fn Agent_OnLoad(vm: *mut jvmti::JavaVM, options: *mut ::std::os::raw::c_char,
                            reserved: *mut ::std::os::raw::c_void) -> jvmti::jint {
-    unsafe {
-        //        pub GetEnv: ::std::option::Option<unsafe extern "C" fn(vm: *mut JavaVM,
-        //                                                               penv:
-        //                                                               *mut *mut ::std::os::raw::c_void,
-        //                                                               version: jint)
-        let mut penv: *mut ::std::os::raw::c_void = ptr::null_mut();
-        (**vm).GetEnv.unwrap()(vm, &mut penv, jvmti::JVMTI_VERSION as i32);
-    }
+    let tienv = env::JVMTIEnv::new(vm);
 
     0
 }
-
 
 #[cfg(test)]
 mod tests {
