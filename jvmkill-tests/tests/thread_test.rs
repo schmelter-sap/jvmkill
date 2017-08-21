94bd8bbb73c0e2a5fcfb331182b360b46b1ed9da
Glyn Normington <gnormington@pivotal.io>
2017-08-21 10:46:14 +0100

    Revert workaround and improve test stability
    ... hopefully. My current theory is that attempting to print memory usage or
    the heap histogram causes the container to hit its memory limit. This only
    occurs on thread exhaustion tests, so isn't so critical.

--- jvmkill-tests/tests/thread_test.rs
+++ jvmkill-tests/tests/thread_test.rs
@@ -31,12 +31,12 @@ fn print_memory_usage_0() {
 
 #[test]
 fn time_10_count_2() {
-    assert!(!run_java("org.cloudfoundry.jvmkill.ThreadExhaustion", "=time=10,count=2,printHeapHistogram=1,heapHistogramMaxEntries=10,printMemoryUsage=0",
+    assert!(!run_java("org.cloudfoundry.jvmkill.ThreadExhaustion", "=time=10,count=2,printHeapHistogram=0,printMemoryUsage=0",
                       &[], &["ResourceExhausted! (1/2)", "jvmkill killing current process"]));
 }
 
 #[test]
 fn parallel_time_10_count_2() {
-    assert!(!run_java("org.cloudfoundry.jvmkill.ParallelThreadExhaustion", "=time=10,count=2", &[],
+    assert!(!run_java("org.cloudfoundry.jvmkill.ParallelThreadExhaustion", "=time=10,count=2,printHeapHistogram=0,printMemoryUsage=0", &[],
                       &["ResourceExhausted! (1/2)", "jvmkill killing current process"]));
 }
