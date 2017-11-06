7dbc0acfde9e1867ffbafde0af89c0fbcc4a6575
Glyn Normington <gnormington@pivotal.io>
2017-11-06 11:42:45 +0000

    Add hints when memory pools are nearly full
    See https://docs.google.com/document/d/1IA26pZ8XsUSnQHZ5bEQ_2-Ihr3GdWH4jr-SW81XtNM8/edit for background.

--- jvmkill/src/agentcontroller/poolstats.rs
+++ jvmkill/src/agentcontroller/poolstats.rs
@@ -13,12 +13,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use std::collections::HashMap;
+use std::io::Write;
 
-pub struct PoolStats {}
+pub struct PoolStats {
+    pool_hint: HashMap<String, String>
+}
 
 impl PoolStats {
     pub fn new() -> Self {
-        Self {}
+        let mut pool_hint = HashMap::new();
+        pool_hint.insert(String::from("Heap memory"), String::from("increase the container size"));
+        pool_hint.insert(String::from("Metaspace"), String::from("set -XX:MaxMetaspaceSize to a suitable value"));
+        pool_hint.insert(String::from("Compressed Class Space"), String::from("set -XX:CompressedClassSpaceSize to a suitable value"));
+        Self {
+            pool_hint: pool_hint
+        }
+    }
+
+    fn print_stats(&self, name: String, stats: Stats, output: &mut Write) {
+        let (stats_line, pool_nearly_full) = usage_stats(stats);
+        writeln_paced!(output, "   {}: {}", name, stats_line);
+        if pool_nearly_full {
+            let sw = self.pool_hint.get(&name);
+            if let Some(sw_string) = sw {
+                writeln_paced!(output, "      Hint: {} is over 95% full. To increase it, {}.", name, sw_string);
+            }
+        }
     }
 }
 
@@ -36,7 +57,11 @@ impl super::Action for PoolStats {
             return Err(::err::Error::ActionUnavailableOnThreadExhaustion("determine memory usage statistics".to_string()));
         }
 
+        let mut output = &mut ::std::io::stdout();
+
         let mf_class = jni_env.find_class("java/lang/management/ManagementFactory")?;
+
+        writeln_paced!(output, "\nMemory usage:");
         let get_memory_mxbean_method_id = jni_env.get_static_method_id(mf_class, "getMemoryMXBean", "()Ljava/lang/management/MemoryMXBean;")?;
         let memory_mxbean = jni_env.call_static_object_method(mf_class, get_memory_mxbean_method_id)?;
 
@@ -44,14 +69,11 @@ impl super::Action for PoolStats {
 
         let heap_memory_usage_method_id = jni_env.get_method_id(memory_mxbean_class, "getHeapMemoryUsage", "()Ljava/lang/management/MemoryUsage;")?;
         let heap_usage = jni_env.call_object_method(memory_mxbean, heap_memory_usage_method_id)?;
+        self.print_stats(String::from("Heap memory"), usage_statistics(jni_env, heap_usage)?, &mut output);
 
         let non_heap_memory_usage_method_id = jni_env.get_method_id(memory_mxbean_class, "getNonHeapMemoryUsage", "()Ljava/lang/management/MemoryUsage;")?;
         let non_heap_usage = jni_env.call_object_method(memory_mxbean, non_heap_memory_usage_method_id)?;
-
-        let heap_usage_stats = usage_stats(jni_env, heap_usage)?;
-        let non_heap_usage_stats = usage_stats(jni_env, non_heap_usage)?;
-        let output = &mut ::std::io::stdout();
-        writeln_paced!(output, "\nMemory usage:\n   Heap memory: {}\n   Non-heap memory: {}", heap_usage_stats, non_heap_usage_stats);
+        self.print_stats(String::from("Non-heap memory"), usage_statistics(jni_env, non_heap_usage)?, &mut output);
 
         writeln_paced!(output, "\nMemory pool usage:");
 
@@ -70,24 +92,83 @@ impl super::Action for PoolStats {
             let name = jni_env.call_object_method(pool_mxbean, get_name_method_id)? as ::jvmti::jstring;
             let usage = jni_env.call_object_method(pool_mxbean, get_usage_method_id)?;
             let (name_utf_chars, name_cstr) = jni_env.get_string_utf_chars(name);
-            let stats = usage_stats(jni_env, usage)?;
-            writeln_paced!(output, "   {}: {}", name_cstr.to_string_lossy().into_owned(), stats);
+            let name_string = name_cstr.to_string_lossy().into_owned();
+            let statistics = usage_statistics(jni_env, usage)?;
+
+            self.print_stats(name_string, statistics, &mut output);
             jni_env.release_string_utf_chars(name, name_utf_chars);
         }
         Ok(())
     }
 }
 
-fn usage_stats(mut jni_env: ::env::JniEnv, usage: ::jvmti::jobject) -> Result<String, ::err::Error> {
+type Stats = (i64, i64, i64, i64);
+
+fn usage_statistics(mut jni_env: ::env::JniEnv, usage: ::jvmti::jobject) -> Result<Stats, ::err::Error> {
     let memory_usage_class = jni_env.get_object_class(usage)?;
     let get_init_method_id = jni_env.get_method_id(memory_usage_class, "getInit", "()J")?;
     let get_used_method_id = jni_env.get_method_id(memory_usage_class, "getUsed", "()J")?;
     let get_committed_method_id = jni_env.get_method_id(memory_usage_class, "getCommitted", "()J")?;
     let get_max_method_id = jni_env.get_method_id(memory_usage_class, "getMax", "()J")?;
 
-    Ok(format!("init {}, used {}, committed {}, max {}",
-               jni_env.call_long_method(usage, get_init_method_id),
-               jni_env.call_long_method(usage, get_used_method_id),
-               jni_env.call_long_method(usage, get_committed_method_id),
-               jni_env.call_long_method(usage, get_max_method_id)))
+    Ok((jni_env.call_long_method(usage, get_init_method_id),
+        jni_env.call_long_method(usage, get_used_method_id),
+        jni_env.call_long_method(usage, get_committed_method_id),
+        jni_env.call_long_method(usage, get_max_method_id)))
+}
+
+fn usage_stats(usage: Stats) -> (String, bool) {
+    let (init, used, committed, max) = usage;
+
+    let nearly_full = (committed as f32) / (max as f32) >= 0.95;
+
+    ((format!("init {}, used {}, committed {}, max {}", init, used, committed, max), nearly_full))
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn no_hint_by_default() {
+        let mut output = Vec::new();
+
+        let pool_stats = super::PoolStats::new();
+        pool_stats.print_stats(String::from("some pool"), (0i64, 1i64, 95i64, 100i64), &mut output);
+        assert_eq!(String::from_utf8(output).unwrap(), "   some pool: init 0, used 1, committed 95, max 100\n")
+    }
+
+    #[test]
+    fn no_heap_hint_by_default() {
+        let mut output = Vec::new();
+
+        let pool_stats = super::PoolStats::new();
+        pool_stats.print_stats(String::from("Heap memory"), (0i64, 1i64, 94i64, 100i64), &mut output);
+        assert_eq!(String::from_utf8(output).unwrap(), "   Heap memory: init 0, used 1, committed 94, max 100\n")
+    }
+
+    #[test]
+    fn heap_hint_when_nearly_full() {
+        let mut output = Vec::new();
+
+        let pool_stats = super::PoolStats::new();
+        pool_stats.print_stats(String::from("Heap memory"), (0i64, 1i64, 95i64, 100i64), &mut output);
+        assert_eq!(String::from_utf8(output).unwrap(), "   Heap memory: init 0, used 1, committed 95, max 100\n      Hint: Heap memory is over 95% full. To increase it, increase the container size.\n")
+    }
+
+    #[test]
+    fn metaspace_hint_when_nearly_full() {
+        let mut output = Vec::new();
+
+        let pool_stats = super::PoolStats::new();
+        pool_stats.print_stats(String::from("Metaspace"), (0i64, 1i64, 95i64, 100i64), &mut output);
+        assert_eq!(String::from_utf8(output).unwrap(), "   Metaspace: init 0, used 1, committed 95, max 100\n      Hint: Metaspace is over 95% full. To increase it, set -XX:MaxMetaspaceSize to a suitable value.\n")
+    }
+
+    #[test]
+    fn compressed_class_space_hint_when_nearly_full() {
+        let mut output = Vec::new();
+
+        let pool_stats = super::PoolStats::new();
+        pool_stats.print_stats(String::from("Compressed Class Space"), (0i64, 1i64, 95i64, 100i64), &mut output);
+        assert_eq!(String::from_utf8(output).unwrap(), "   Compressed Class Space: init 0, used 1, committed 95, max 100\n      Hint: Compressed Class Space is over 95% full. To increase it, set -XX:CompressedClassSpaceSize to a suitable value.\n")
+    }
 }
