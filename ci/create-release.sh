8d5cdfff3e90e663f49756218b5997abcc9eea61
Ben Hale <bhale@pivotal.io>
2018-05-31 13:43:06 -0700

    Add Bionic Support

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -14,6 +14,7 @@ update_release() {
   sed -E -i '' "s|(^[ ]*VERSION:[ ]*).+$|\1$version|" $file
 }
 
+update_release ci/deploy-bionic.yml libs-release-local $RELEASE
 update_release ci/deploy-osx.yml libs-release-local $RELEASE
 update_release ci/deploy-trusty.yml libs-release-local $RELEASE
 git add .
@@ -22,6 +23,7 @@ git commit --message "v$RELEASE Release"
 git tag -s v$RELEASE -m "v$RELEASE"
 git reset --hard HEAD^1
 
+update_release ci/deploy-bionic.yml libs-snapshot-local $SNAPSHOT
 update_release ci/deploy-osx.yml libs-snapshot-local $SNAPSHOT
 update_release ci/deploy-trusty.yml libs-snapshot-local $SNAPSHOT
 git add .
